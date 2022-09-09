f252641b00ff94a32183d1397c72a234680bb597
solcany <matus.solcany@protonmail.com>
2022-09-09 12:49:36 +0200

    remove thread sleep

--- one_photo/src/main.rs
+++ one_photo/src/main.rs
@@ -6,9 +6,11 @@ use rascam;
 use std::io::Write;
 
 fn main() {
-	const DELAY: std::time::Duration = time::Duration::from_secs(3);	
-	const PATH: &str = "./images";	
+	const DELAY: std::time::Duration = time::Duration::from_millis(10);	
+	const PATH: &str = "images";	
 	const FILE_FORMAT: &str = "jpg";
+	const COMMAND_START: &str = "scp m@192.168.1.77:~/Documents/rust/sky_watch/one_photo/";
+	const COMMAND_END:  &str = " ~/Desktop";
 
 	// get information about all installed cameras
     let info = rascam::info().unwrap();
@@ -32,16 +34,17 @@ fn main() {
 	// the time is image's file name
 	let image_path = format!("{}/{}.{}", PATH, time.format("%H_%M_%S_%m_%d"), FILE_FORMAT);
 
-	thread::sleep(DELAY);
+//	thread::sleep(DELAY);
 
 	// take photo
 	let photo = camera.take_one().unwrap();
 
-	thread::sleep(DELAY);	
+//	thread::sleep(DELAY);	
 
 	// create and save the image file
 	fs::File::create(&image_path).unwrap().write_all(&photo).unwrap();
 
 	println!("image '{}' saved", &image_path);
+	println!("{}{}/{}", COMMAND_START, &image_path, COMMAND_END);
 
 }
