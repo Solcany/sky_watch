c23b68a9e556423f5f4d5de8aac21ed5d647918c
m <matus.solcany@protonmail.com>
2022-09-15 15:04:02 +0200

    rename func

--- csv_writer/src/main.rs
+++ csv_writer/src/main.rs
@@ -1,3 +1,14 @@
+use std::error::Error;
+use csv::Writer;
+
+fn write_csv() -> Result<(), Box<dyn Error>> {
+    let mut wtr = Writer::from_path("foo.csv")?;
+    wtr.write_record(&["a", "b", "c"])?;
+    wtr.write_record(&["x", "y", "z"])?;
+    wtr.flush()?;
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    write_csv().unwrap();
 }
