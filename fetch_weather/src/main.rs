21cbb98503cb1d316ec9c8126185606c3d037b24
m <matus.solcany@protonmail.com>
2023-02-03 18:20:20 +0100

    fetch parse json

--- fetch_weather/src/main.rs
+++ fetch_weather/src/main.rs
@@ -2,17 +2,15 @@ use reqwest::blocking::get;
 use serde_json::Value;
 use serde::Serialize;
 use std::path::Path;
-
 use csv::Writer;
 use std::fs::OpenOptions;
+use std::time::Duration;
+use std::thread::sleep;
 
+const FETCH_DELAY_SECONDS : u32 = 4;
 const OUT_PATH : &str = "./";
 const CSV_NAME : &str = "data.csv";
-const CSV_HEADER_LABELS : [&str; 5]= ["air_pressure", 
-                                      "temperature",
-                                      "feel_temperature",
-                                      "weather_description",
-                                      "timestamp"];
+
 #[derive(Serialize)]
 struct Row<'a> {
     air_pressure: f64,
@@ -49,55 +47,54 @@ fn fetch_eindhoven_weather_data() -> Option<Value> {
 
 
 fn main() {
-    let csv_path : String = format!("{}/{}", OUT_PATH, CSV_NAME);
-
-    // // initialise the csv writer
-    // let mut writer = match Path::new(&csv_path).exists() {
-    //     // if there's existing csv append to new rows to it
-    //     true => { 
-    //                 let csv_file = OpenOptions::new()
-    //                     .write(true)
-    //                     .create(true)
-    //                     .append(true)
-    //                     .open(csv_path)
-    //                     .unwrap();
-    //                 Writer::from_writer(csv_file);
-    //     },
-    //     // otherwise create new csv file
-    //     false => { 
-    //                 Writer::from_path(csv_path)
-    //                     .unwrap()
-    //                     .write_record(CSV_HEADER_LABELS);
-    //             },
-    // }
-
+    let csv_path : String = format!("{}/{}", OUT_PATH, CSV_NAME);    
     let mut writer;
-
-    let csv_file = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .append(true)
-        .open(csv_path)
-        .unwrap();
-
-    writer = Writer::from_writer(csv_file);
-
-    // fetch data from the api
-    let eindhoven_weather_data = fetch_eindhoven_weather_data();
-
-    match eindhoven_weather_data {
-        Some(data) => {
-            // write new row
-            let row : Row = Row {  
-                               air_pressure: data["airpressure"].as_f64().unwrap(),
-                               temperature: data["temperature"].as_f64().unwrap(),
-                               feel_temperature: data["feeltemperature"].as_f64().unwrap(),
-                               weather_description: data["weatherdescription"].as_str().unwrap(),
-                               timestamp: data["timestamp"].as_str().unwrap(),
-                            };
-            writer.serialize(row).unwrap();
-            writer.flush().unwrap();
-        },
-        None => println!("{}", "Eindhoven data not found"),
+    // initialise the csv writer
+    if Path::new(&csv_path).exists() {
+        // if there's existing csv on the path append new rows to it
+        let csv_file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .append(true)
+            .open(csv_path)
+            .unwrap();
+        writer = Writer::from_writer(csv_file);
+    } else {
+        // otherwise create new csv file 
+        writer = Writer::from_path(csv_path).unwrap();
     }
+
+    let fetch_delay = Duration::from_secs(FETCH_DELAY_SECONDS as u64);
+    let mut c : u32 = 0;
+
+    loop {
+        println!("row : {}", c);
+        if c == 5 {
+            println!("done!");
+            break;
+        }
+
+        // fetch data from the api
+        let eindhoven_weather_data = fetch_eindhoven_weather_data();
+
+        match eindhoven_weather_data {
+            Some(data) => {
+                // write new row
+                let row : Row = Row {  
+                                   air_pressure: data["airpressure"].as_f64().unwrap(),
+                                   temperature: data["temperature"].as_f64().unwrap(),
+                                   feel_temperature: data["feeltemperature"].as_f64().unwrap(),
+                                   weather_description: data["weatherdescription"].as_str().unwrap(),
+                                   timestamp: data["timestamp"].as_str().unwrap(),
+                                };
+                writer.serialize(row).unwrap();
+                writer.flush().unwrap();
+            },
+            None => println!("{}", "Eindhoven data not foundß"),
+        }
+
+        sleep(fetch_delay);
+        c += 1;        
+    }
+
 }
